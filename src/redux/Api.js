56e890bf1b1b9b1eff184d924642f2795c0c1f84
AjayRajput17 <ajayingle3878@gmail.com>
2025-03-16 16:49:30 +0530

    proxy added

--- src/redux/Api.js
+++ src/redux/Api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: "https://blog-server-sa7e.onrender.com",
+  // baseURL: "https://blog-server-sa7e.onrender.com",
+  baseURL: "http://3.109.155.96:5000",
   withCredentials: true,
 })
 
